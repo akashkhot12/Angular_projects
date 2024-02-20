1a57621321a56b5e3cc7e746e229fce3f161f49a
akashkhot12 <akashkhot03@gmail.com>
2024-02-20 12:26:26 +0530

    adding forms

--- Crud-Operations-Angular/src/app/user/user.component.ts
+++ Crud-Operations-Angular/src/app/user/user.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  userForms: any;
+
+constructor(public fb:FormBuilder){
+  this.userForms = this.fb.group({
+    Name:[""],
+    Email:[""],
+    Mobile:[""],
+    Age:[""],
+
+  })
+
+  
+}
+
+submitForm(){
+  console.log(this.userForms.value);
+  
+}
+
+  ngOnInit(): void {
+   
+  }
 
 }
