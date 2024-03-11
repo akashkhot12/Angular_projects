9dc2b7fd943395fc4796f3238f551867a0f4ecde
akashkhot12 <akashkhot03@gmail.com>
2024-03-11 15:11:26 +0530

    data into the storage

--- Login-signUp/src/app/login/login/login.component.ts
+++ Login-signUp/src/app/login/login/login.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  signupUsers:any[]=[];
+  singupObj:any ={
+    userName :'',
+    email:'',
+    password:'',
+    number:'',
+    gender:''
+    };
 
+    loginObj:any ={
+      userName :'',
+      password:'',
+      };
+
+      constructor(){}
+
+      ngOnInit(): void {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        
+      }
+
+      onSingUp(){
+
+      }
+
+      onLogin(){
+        
+      }
 }
